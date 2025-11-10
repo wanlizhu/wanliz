fdec42e58144aecb55e150e1c765f0a34f7c577e
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-09 17:28:09 -0800

    s

--- nvperf.py
+++ nvperf.py
@@ -588,7 +588,7 @@ class CMD_upload:
         
     def test(self, user, host, passwd):
         sshpass = f"sshpass -p '{passwd}'" if passwd else ""
-        print(f"Authenticating {user}@{host} with {"password" if passwd else "keys"}")
+        print(f"Authenticating {user}@{host} with {'password' if passwd else 'keys'}")
         output = subprocess.run(["bash", "-lc", rf"""
             {sshpass} ssh -o StrictHostKeyChecking=accept-new -o ConnectTimeout=5 {user}@{host} 'cmd /c exit 0'
         """], check=False)
