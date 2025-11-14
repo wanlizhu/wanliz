bedfccb55524adaea533ee59c3cc2a605c196c7d
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-13 16:05:38 -0800

    s

--- nvperf.py
+++ nvperf.py
@@ -591,7 +591,7 @@ class CMD_ip:
         print(self.public_ip_to("1.1.1.1", missing_OK=False))
 
     def public_ip_to(self, remote, missing_OK):
-        output = subprocess.run(["bash", "-lic", rf"ip -4 route get \"$(getent ahostsv4 {remote} | awk 'NR==1{{print $1}}')\" | sed -n 's/.* src \([0-9.]\+\).*/\1/p'"], check=True, text=True, capture_output=True)
+        output = subprocess.run(["bash", "-lic", rf"ip -4 route get $(getent ahostsv4 {remote} | awk 'NR==1{{print $1}}') | sed -n 's/.* src \([0-9.]\+\).*/\1/p'"], check=True, text=True, capture_output=True)
         if output.returncode == 0 and output.stdout:
             return output.stdout 
         if not missing_OK: 
