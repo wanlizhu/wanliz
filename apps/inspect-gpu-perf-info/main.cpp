ef5413f5ba8ccbe811f3d71a324e9948abac3c04
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-19 18:44:19 -0800

    s

--- apps/inspect-gpu-perf-info/main.cpp
+++ apps/inspect-gpu-perf-info/main.cpp
@@ -2,5 +2,16 @@
 
 int main(int argc, char **argv) {
     std::cout << VK_physdev::INFO() << std::endl;
+#ifdef __linux__
+    if (argc > 1) {
+        pid_t childProc = fork();
+        if (childProc == 0) { // Inside child process
+            
+        } else if (childProc > 0) { // Inside parent process
+            // TODO: communicate with child proc
+        }
+    }
+#endif 
+
     return 0;
 }
