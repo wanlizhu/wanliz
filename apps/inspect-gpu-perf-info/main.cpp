d8865ae321b5415b87121ea3bd1f81e17a8d6527
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-21 13:21:25 -0800

    s

--- apps/inspect-gpu-perf-info/main.cpp
+++ apps/inspect-gpu-perf-info/main.cpp
@@ -6,8 +6,8 @@ const char* realpath(const char* name) {
     if (pipe) {
         static char buf[4096] = {};
         memset(buf, 0, sizeof(buf));
-        int n = fgets(buf, sizeof(buf), pipe);
-        if (buf[n - 1] == '\n') {
+        int n = strlen(fgets(buf, sizeof(buf), pipe));
+        if (n > 0 && buf[n - 1] == '\n') {
             buf[n - 1] = '\0';
         }
         pclose(pipe);
