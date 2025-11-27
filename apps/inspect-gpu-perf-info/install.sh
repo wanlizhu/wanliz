e24eb68d83e8d7ad53f10a8c3f6fa718e4fa9a7a
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-26 17:10:27 -0800

    auto push from Wanlis-ThinkPad at Wed Nov 26 17:10:27 PST 2025

--- apps/inspect-gpu-perf-info/install.sh
+++ apps/inspect-gpu-perf-info/install.sh
@@ -4,5 +4,5 @@ workspace=$(realpath $(dirname $0))
 outdir=$workspace/_out/Linux_$(uname -m | sed 's/x86_64/amd64/g')_release
 mkdir -p $outdir 
 cd $outdir  
-cmake ../..  
+cmake ../.. || exit 1
 sudo cmake --build . --target install || cmake --build .
