be964aadf3368b7a5d80c8b246a32c2dba84f45a
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-26 22:53:48 -0800

    auto push from Wanlis-ThinkPad at Wed Nov 26 22:53:47 PST 2025

--- apps/inspect-gpu-perf-info/VkLayer_hooked_mem_alloc.cpp
+++ apps/inspect-gpu-perf-info/VkLayer_hooked_mem_alloc.cpp
@@ -8,8 +8,16 @@ VKAPI_ATTR VkResult VKAPI_CALL HKed_vkAllocateMemory(
 ) {
     VK_DEFINE_ORIGINAL_FUNC(vkAllocateMemory);
 
+    setenv("__GL_DEBUG_MASK", "RM", 1);
+    setenv("__GL_DEBUG_LEVEL", "30", 1);
+    setenv("__GL_DEBUG_OPTIONS", "LOG_TO_FILE", 1);
+    setenv("__GL_DEBUG_FILENAME", "/tmp/rm-api-loggings", 1);
+
     VkResult result = original_pfn_vkAllocateMemory(device, pAllocateInfo, pAllocator, pMemory);
-    exit(0);
+    
+    if (std::filesystem::exists("/usr/local/bin/process-vidheap.py")) {
+        system("process-vidheap.py /tmp/rm-api-loggings");
+    }
 
     return result;
 }
