9c86ca78a1642727ef1496136d6e311624e6a9d6
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-27 02:10:57 -0800

    s

--- apps/inspect-gpu-perf-info/install.sh
+++ apps/inspect-gpu-perf-info/install.sh
@@ -5,4 +5,4 @@ outdir=$workspace/_out/Linux_$(uname -m | sed 's/x86_64/amd64/g')_release
 mkdir -p $outdir 
 cd $outdir  
 cmake ../.. || exit 1
-sudo cmake --build . --target install || cmake --build .
+sudo cmake --build . --target install 
