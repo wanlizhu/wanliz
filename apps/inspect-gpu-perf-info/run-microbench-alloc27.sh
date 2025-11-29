2c0c7b169272723758c4c2abd95ceab9c37ce565
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-29 18:49:03 +0000

    auto push from 4u2g-0110 at Sat Nov 29 06:49:03 PM UTC 2025

--- apps/inspect-gpu-perf-info/run-microbench-alloc27.sh
+++ apps/inspect-gpu-perf-info/run-microbench-alloc27.sh
@@ -8,4 +8,33 @@ if [[ ! -e "$nvperf_vulkan_path" ]]; then
     exit 1
 fi 
 
-sudo env DEBUG_MEM_ALLOC=1 inspect-gpu-perf-info $nvperf_vulkan_path -nullDisplay alloc:27 2>/tmp/igpi.txt
+sudo env DEBUG_MEM_ALLOC=1 inspect-gpu-perf-info $nvperf_vulkan_path -nullDisplay alloc:27 2> /tmp/igpi.txt
+
+if [[ -f /tmp/igpi.txt ]]; then 
+    vkalloc_logs=
+    vkalloc_logs_begin=
+    while IFS= read line; do 
+        case "$line" in 
+            "vkAllocateMemory BEGIN"*)
+                echo "$line"
+                vkalloc_logs=""
+                vkalloc_logs_begin=1
+            ;;
+            "vkAllocateMemory ENDED"*)
+                echo "$line"
+                vkalloc_logs_begin=0
+                printf "%s\n" "$vkalloc_logs" > /tmp/vkalloc_logs
+                $(realpath $(dirname $0))/process-vidheap.py /tmp/vkalloc_logs
+            ;;
+            *)
+                if [[ "$vkalloc_logs_begin" = 1 ]]; then
+                    vkalloc_logs="$vkalloc_logs$line"$'\n'
+                else
+                    echo "$line"
+                fi 
+            ;;
+        esac
+    done < /tmp/igpi.txt > /tmp/igpi_processed.txt
+    sudo mv -f /tmp/igpi_processed.txt $HOME/igpi_mb_alloc27.txt
+    echo "Logs dumped to $HOME/igpi_mb_alloc27.txt"
+fi 
