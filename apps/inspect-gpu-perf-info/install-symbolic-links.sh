e93b1411bf1d0b9e6560f0ad0a37d2ea63fbe279
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-29 19:18:05 +0000

    auto push from 4u2g-0110 at Sat Nov 29 07:18:05 PM UTC 2025

--- apps/inspect-gpu-perf-info/install-symbolic-links.sh
+++ apps/inspect-gpu-perf-info/install-symbolic-links.sh
@@ -7,7 +7,7 @@ cmake ../.. || exit 1
 sudo cmake --build . --config debug || exit 1
 sudo ln -sfv $outdir/inspect-gpu-perf-info /usr/local/bin/inspect-gpu-perf-info || exit 1
 sudo ln -sfv $outdir/VkLayer_inspect_gpu_perf_info.json /usr/share/vulkan/explicit_layer.d/VkLayer_inspect_gpu_perf_info.json || exit 1
-sudo ln -sfv $(realpath $(dirname $0))/merge-gpu-pages.sh /usr/local/bin/merge-gpu-pages.sh || exit 1
+sudo ln -sfv $(realpath $outdir/../..)/merge-gpu-pages.sh /usr/local/bin/merge-gpu-pages.sh || exit 1
 
 if [[ -z $(which inspect-gpu-page-tables) ]]; then 
     echo "Action required to install inspect-gpu-page-tables"
