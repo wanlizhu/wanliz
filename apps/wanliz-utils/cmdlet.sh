c416a3e0d21a6d6474b0ea555f1ad9cc8f2a9ab4
Wanli Zhu <zhu.wanli@icloud.com>
2025-11-30 16:06:39 -0800

    s

--- apps/wanliz-utils/cmdlet.sh
+++ apps/wanliz-utils/cmdlet.sh
@@ -12,5 +12,39 @@ case $1 in
         echo "Install Nsight graphics: https://ngfx/builds-prerel/Grxa/"
         echo "Install Nsight graphics: https://ngfx/builds-nightly/Grfx/"
         echo "Install Nsight graphics: \\devrel\share\Devtools\NomadBuilds\latest\Internal (username is email)"
+        echo 
+        echo "Launch Nsight systems from command line: "
+        if [[ -e ~/nsight_systems/bin/nsys ]]; then 
+            sampling_period=$($HOME/nsight_systems/bin/nsys profile --help=sampling 2>/dev/null | awk '
+                /--sampling-period=/ {flag=1; next}
+                flag && /Possible values are integers between/ {
+                    c=0
+                    for (i=1; i<=NF; i++) {
+                        s=$i
+                        gsub(/[^0-9]/, "", s)
+                        if (s != "") { nums[++c]=s }
+                    }
+                    if (c >= 2) {
+                        min=nums[1]
+                        if (nums[2] < min) min=nums[2]
+                        print min
+                    }
+                    exit
+                }')
+            metrics_freq=$($HOME/nsight_systems/bin/nsys profile --help=gpu 2>/dev/null | awk '
+                /--gpu-metrics-frequency=/ {flag=1; next}
+                flag && /Maximum supported frequency is/ {
+                    s=$0
+                    gsub(/[^0-9]/, "", s)
+                    print s
+                    exit
+                }')
+            echo ">> sudo $HOME/nsight_systems/bin/nsys profile --trace=vulkan,opengl,cuda,nvtx,osrt --vulkan-gpu-workload=individual --sample=process-tree --sampling-period=$sampling_period --samples-per-backtrace=1 --backtrace=dwarf --cpuctxsw=process-tree --syscall=process-tree --gpu-metrics-devices=all --gpu-metrics-frequency=$metrics_freq --stats=true --export=sqlite,text --resolve-symbols=true --force-overwrite=true --stop-on-exit=true --wait=all --output=nsys_ --show-output=Nsys_$(hostname)_$(date +%Y%m%d) <exe-and-args>" 
+        else
+            echo -e "\tError: ~/nsight_systems/bin/nsys doesn't exist"
+        fi 
+        echo 
+        echo "Launch Nsight graphics from command line:"
+        echo -e "\tTODO"
     ;;
 esac 
