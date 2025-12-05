3906790cdb72d57ea1ffec96985a73a92b6e6ff0
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-04 16:45:56 -0800

    s

--- apps/inspect-gpu-perf-info/scripts/install-symbolic-links.sh
+++ apps/inspect-gpu-perf-info/scripts/install-symbolic-links.sh
@@ -8,4 +8,4 @@ sudo cmake --build . --config debug || exit 1
 sudo ln -sfv $outdir/inspect-gpu-perf-info /usr/local/bin/inspect-gpu-perf-info || exit 1
 sudo ln -sfv $outdir/VK_LAYER_igpi_helper.json /usr/share/vulkan/explicit_layer.d/VK_LAYER_igpi_helper.json || exit 1
 
-$(dirname $0)/install-gpu-pages-inspector.sh 
+$(realpath $(dirname $0))/install-gpu-pages-inspector.sh 
