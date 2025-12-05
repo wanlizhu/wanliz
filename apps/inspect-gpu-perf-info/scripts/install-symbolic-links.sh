23aea353813eb7b2d080384830a1f8f79ef1b6b2
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-04 16:49:30 -0800

    s

--- apps/inspect-gpu-perf-info/scripts/install-symbolic-links.sh
+++ apps/inspect-gpu-perf-info/scripts/install-symbolic-links.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
-mkdir -p $(dirname $0)/../_out/Linux_debug
-outdir=$(realpath $(dirname $0)/../_out/Linux_debug)
-cd $outdir || exit 1
+cd $HOME/wanliz/apps/inspect-gpu-perf-info
+mkdir -p _out/Linux_debug
+cd _out/Linux_debug  
 cmake ../.. || exit 1
 sudo cmake --build . --config debug || exit 1
-sudo ln -sfv $outdir/inspect-gpu-perf-info /usr/local/bin/inspect-gpu-perf-info || exit 1
-sudo ln -sfv $outdir/VK_LAYER_igpi_helper.json /usr/share/vulkan/explicit_layer.d/VK_LAYER_igpi_helper.json || exit 1
+sudo ln -sfv $(pwd)/inspect-gpu-perf-info /usr/local/bin/inspect-gpu-perf-info || exit 1
+sudo ln -sfv $(pwd)/VK_LAYER_igpi_helper.json /usr/share/vulkan/explicit_layer.d/VK_LAYER_igpi_helper.json || exit 1
 
-$(realpath $(dirname $0))/install-gpu-pages-inspector.sh 
+$HOME/wanliz/apps/inspect-gpu-perf-info/scripts/install-gpu-pages-inspector.sh 
