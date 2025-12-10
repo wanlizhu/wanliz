25cb66872b335c6d8a42133a49f05b8792c3fe77
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-10 01:32:37 -0800

    s

--- wanliz-cmd-builder.sh
+++ wanliz-cmd-builder.sh
@@ -1,1 +1,8 @@
 #!/usr/bin/env bash
+
+if [[ $1 == p4env ]]; then 
+    echo 'export P4PORT="p4proxy-sc.nvidia.com:2006"
+export P4USER="wanliz"
+export P4CLIENT="wanliz_sw_linux"
+export P4ROOT="/wanliz_sw_linux"'
+fi 
