c1f824e2fda092f9e3ec7c01242c3cf3a560bd56
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-17 10:00:24 -0800

    s

--- wanliz-nvsrc-version.sh
+++ wanliz-nvsrc-version.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ -d /wanliz_sw_linux ]]; then 
-    export NV_SOURCE="/wanliz_sw_linux/dev/gpu_drv/bugfix_main"
-elif [[ -d /wanliz_sw_windows_wsl2 ]]; then 
-    export NV_SOURCE="/wanliz_sw_windows_wsl2/workingbranch"
-fi 
-
-cat $NV_SOURCE/drivers/common/inc/nvUnixVersion.h | grep '#define' | grep NV_VERSION_STRING | awk -F'"' '{print $2}'
+cat /wanliz_sw_windows_wsl2/workingbranch/drivers/common/inc/nvUnixVersion.h | grep '#define' | grep NV_VERSION_STRING | awk -F'"' '{print $2}'
