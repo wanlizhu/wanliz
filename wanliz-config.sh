20eccd7cd87e45f475b0b72b4aa49a376938a2fe
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-21 12:03:24 -0800

    s

--- wanliz-config.sh
+++ wanliz-config.sh
@@ -1,21 +1,13 @@
 #!/usr/bin/env bash
 trap 'exit 130' INT
 
-YES_FOR_ALL=
-if [[ $1 == -y ]]; then 
-    YES_FOR_ALL=1
-fi 
-
 if [[ $EUID != 0 ]]; then 
-    [[ -z $YES_FOR_ALL ]] && read -p "Set up no-password sudo for $USER? [Y/n]: " nopasswd_sudo || nopasswd_sudo=
-    if [[ -z $nopasswd_sudo || $nopasswd_sudo =~ ^([yY]([eE][sS])?)?$ ]]; then 
-        if ! sudo grep -qxF "$USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then 
+    if ! sudo grep -qxF "$USER ALL=(ALL) NOPASSWD:ALL" /etc/sudoers; then 
+        read -p "Enable no-password sudo for $USER? [Y/n]: " nopasswd_sudo 
+        if [[ -z $nopasswd_sudo || $nopasswd_sudo =~ ^([yY]([eE][sS])?)?$ ]]; then 
             echo "$USER ALL=(ALL) NOPASSWD:ALL" | sudo tee -a /etc/sudoers &>/dev/null
-            echo "[OK]"
-        else
-            echo "[SKIPPED]"
-        fi
-    fi 
+        fi 
+    fi
 fi 
 
 if ! grep -q "wanliz utils hosts" /etc/hosts; then 
@@ -28,8 +20,8 @@ fi
 
 if [[ ! -f $HOME/.ssh/config  ]]; then 
     read -p "Add configs to ~/.ssh/config? [Y/n]: " ssh_config
-    mkdir -p $HOME/.ssh 
     if [[ -z $ssh_config || $ssh_config =~ ^([yY]([eE][sS])?)?$ ]]; then 
+        mkdir -p $HOME/.ssh 
         echo >> $HOME/.ssh/config
         cat $HOME/wanliz/ssh-config.txt >> $HOME/.ssh/config
     fi 
@@ -39,12 +31,12 @@ if [[ ! -f $HOME/.ssh/id_ed25519 ]]; then
     read -p "Restore wanliz's SSH ID? [Y/n]: " ssh_id
     if [[ -z $ssh_id || $ssh_id =~ ^([yY]([eE][sS])?)?$ ]]; then 
         mkdir -p $HOME/.ssh 
-        rsync -ah wanliz@office:/home/wanliz/.ssh/id_ed25519     $HOME/.ssh/id_ed25519
-        rsync -ah wanliz@office:/home/wanliz/.ssh/id_ed25519.pub $HOME/.ssh/id_ed25519.pub
+        rsync -ah wanliz@xterm:/home/wanliz/.ssh/id_ed25519     $HOME/.ssh/id_ed25519
+        rsync -ah wanliz@xterm:/home/wanliz/.ssh/id_ed25519.pub $HOME/.ssh/id_ed25519.pub
     fi 
 fi 
 
-[[ -z $YES_FOR_ALL ]] && read -p "Install profiling packages? [Y/n]: " install_pkg || install_pkg=
+read -p "Install profiling packages? [Y/n]: " install_pkg 
 if [[ -z $install_pkg || $install_pkg =~ ^([yY]([eE][sS])?)?$ ]]; then 
     python_version=$(python3 -c 'import sys; print(f"{sys.version_info[0]}.{sys.version_info[1]}")')
     for pkg in python${python_version}-dev python${python_version}-venv \
@@ -76,7 +68,7 @@ if [[ -z $install_pkg || $install_pkg =~ ^([yY]([eE][sS])?)?$ ]]; then
     fi 
 fi 
 
-[[ -z $YES_FOR_ALL ]] && read -p "Install symlinks of profiling scripts to /usr/local/bin? [Y/n]: " install_symlinks || install_symlinks=
+read -p "Install profiling scripts? [Y/n]: " install_symlinks 
 if [[ -z $install_symlinks || $install_symlinks =~ ^([yY]([eE][sS])?)?$ ]]; then 
     find /usr/local/bin -maxdepth 1 -type l -print0 | while IFS= read -r -d '' link; do 
         if real_target=$(readlink -f "$link"); then  
@@ -97,27 +89,22 @@ if [[ -z $install_symlinks || $install_symlinks =~ ^([yY]([eE][sS])?)?$ ]]; then
 fi 
 
 if [[ $USER == wanliz ]]; then 
-    [[ -z $YES_FOR_ALL ]] && read -p "Set up global git options? [Y/n]: " setup_git || setup_git=
-    if [[ -z $setup_git || $setup_git =~ ^([yY]([eE][sS])?)?$ ]]; then 
-        git_email=$(git config --global user.email 2>/dev/null || true)
-        if [[ -z $git_email ]]; then
-            git config --global user.email "zhu.wanli@icloud.com"
-        fi 
-        git_name=$(git config --global user.name 2>/dev/null || true)
-        if [[ -z $git_name ]]; then
-            git config --global user.name "Wanli Zhu"
-        fi 
-        git_editor=$(git config --global core.editor 2>/dev/null || true)
-        if [[ -z $git_editor ]]; then
-            if [[ -z $(which vim) ]]; then 
-                sudo apt install -y vim 2>/dev/null
-            fi 
-            git config --global core.editor "vim"
-        fi
+    git_email=$(git config --global user.email 2>/dev/null || true)
+    if [[ -z $git_email ]]; then
+        git config --global user.email "zhu.wanli@icloud.com"
     fi 
-fi 
+    git_name=$(git config --global user.name 2>/dev/null || true)
+    if [[ -z $git_name ]]; then
+        git config --global user.name "Wanli Zhu"
+    fi 
+    git_editor=$(git config --global core.editor 2>/dev/null || true)
+    if [[ -z $git_editor ]]; then
+        if [[ -z $(which vim) ]]; then 
+            sudo apt install -y vim 2>/dev/null
+        fi 
+        git config --global core.editor "vim"
+    fi
 
-if [[ $USER == wanliz ]]; then 
     if [[ ! -f ~/.vimrc ]]; then 
         cat <<'EOF' > ~/.vimrc
 set expandtab
@@ -145,13 +132,9 @@ if [[ ! -d /mnt/linuxqa/wanliz ]]; then
         if [[ -d /mnt/x/wanliz ]]; then 
             sudo rm -rf /mnt/linuxqa
             sudo ln -sf /mnt/x /mnt/linuxqa
-        else 
-            echo "Todo: run in PowerShell as admin"
-            echo 'Todo: net use X: \\linuxqa.nvidia.com\people /user:wanliz@nvidia.com /persistent:yes'
-            echo 'Todo: then rerun this command'
         fi 
     else 
-        [[ -z $YES_FOR_ALL ]] && read -p "Mount /mnt/linuxqa? [Y/n]: " mount_linuxqa || mount_linuxqa=
+        read -p "Mount /mnt/linuxqa? [Y/n]: " mount_linuxqa 
         if [[ -z $mount_linuxqa || $mount_linuxqa =~ ^([yY]([eE][sS])?)?$ ]]; then 
             echo -n "Mounting /mnt/linuxqa ... "
             if [[ ! -d /mnt/linuxqa ]]; then 
