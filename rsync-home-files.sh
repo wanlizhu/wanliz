c9004952253bbd074171c1df6cba93da54637909
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-24 16:49:26 -0800

    s

--- rsync-home-files.sh
+++ rsync-home-files.sh
@@ -26,6 +26,6 @@ if ((${#home_files[@]})); then
 
     remote_ip=$(cat /tmp/rsync-to-ipv4)
     ssh wanliz@$remote_ip "mkdir -p /mnt/d/${USER}@$(hostname)"
-    rsync -lth --info=progress2 -e 'ssh -o StrictHostKeyChecking=accept-new' "${home_files[@]}" wanliz@$remote_ip:/mnt/d/${USER}@$(hostname)/
+    sudo rsync -lth --info=progress2 -e 'ssh -o StrictHostKeyChecking=accept-new' "${home_files[@]}" wanliz@$remote_ip:/mnt/d/${USER}@$(hostname)/
 fi 
 
