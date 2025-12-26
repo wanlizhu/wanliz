7ac519cf911fb6dd8349776c52a61f6fdaea55ff
Wanli Zhu <zhu.wanli@icloud.com>
2025-12-26 13:57:04 -0800

    s

--- config-env.sh
+++ config-env.sh
@@ -117,25 +117,19 @@ read -p "Install profiling scripts? [Yes/no]: " install_symlinks
 if [[ -z ${install_symlinks//[[:space:]]/} || $install_symlinks =~ ^[[:space:]]*([yY]([eE][sS])?)?[[:space:]]*$ ]]; then
     if [[ $sudo_access == yes ]]; then 
         remove_confirmed=
-        find /usr/local/bin -maxdepth 1 -type l -print0 | while IFS= read -r -d '' link; do 
-            broken_symlink=
-            if ! readlink -f "$link"; then  
-                broken_symlink=1
-            fi 
-            if [[ $broken_symlink == 1 ]]; then 
-                if [[ -z $remove_confirmed ]]; then 
-                    read -p "Remove broken symlinks in /usr/local/bin? [Yes/no]: " remove_confirmed
-                    if [[ -z ${remove_confirmed//[[:space:]]/} || $remove_confirmed =~ ^[[:space:]]*([yY]([eE][sS])?)?[[:space:]]*$ ]]; then
-                        remove_confirmed=yes 
-                    else
-                        remove_confirmed=no
-                    fi 
-                fi 
-                if [[ $remove_confirmed == yes ]]; then 
-                    sudo rm -f "$link" &>/dev/null 
-                fi 
-            fi 
-        done 
+        while IFS= read -r -d '' link; do
+            if [[ -z ${remove_confirmed} ]]; then
+                read -r -p "Remove broken symlinks in /usr/local/bin? [Yes/no]: " remove_confirmed
+                if [[ -z ${remove_confirmed//[[:space:]]/} || $remove_confirmed =~ ^[[:space:]]*([yY]([eE][sS])?)?[[:space:]]*$ ]]; then
+                    remove_confirmed=yes
+                else
+                    remove_confirmed=no
+                fi
+            fi
+            if [[ $remove_confirmed == yes ]]; then
+                sudo rm -f -- "$link" &>/dev/null
+            fi
+        done < <(find /usr/local/bin -maxdepth 1 -xtype l -print0 2>/dev/null)
     fi 
 
     mkdir -p $HOME/.local/bin
